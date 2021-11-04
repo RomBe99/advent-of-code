ab632a9b23b6cc58cee3b59212cb25559cdd9500
Belinsky Roman <romanbelinsky99@gmail.com>
2021-11-05 02:30:44 +0600

    Complete task #1

---
+++ src/main/kotlin/com/rombe/advnt/of/code/day1/ReportParser.kt
@@ -0,0 +1,39 @@
+package com.rombe.advnt.of.code.day1
+
+import java.io.FileNotFoundException
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.notExists
+
+fun findTwoEntriesForSum(fileName: String, sumResult: Int = 2020): Pair<Int, Int> {
+    val pathToData = Path.of(fileName)
+
+    if (pathToData.notExists()) {
+        throw FileNotFoundException("File by $pathToData not found")
+    }
+
+    Files.newBufferedReader(pathToData)?.use { reader ->
+        var number = reader.readLine()?.toInt()
+        val numbers = mutableMapOf<Int, Int>()
+
+        while (number != null) {
+            val temp = numbers[number]
+
+            if (temp == null) {
+                numbers[sumResult - number] = number
+            } else {
+                return number to temp
+            }
+
+            number = reader.readLine()?.toInt()
+        }
+    }
+
+    throw Exception("Can't found number for $sumResult")
+}
+
+fun main() {
+    val result = findTwoEntriesForSum(fileName = "./src/main/resources/input/day1.txt").let { it.first * it.second }
+
+    println(result)
+}
