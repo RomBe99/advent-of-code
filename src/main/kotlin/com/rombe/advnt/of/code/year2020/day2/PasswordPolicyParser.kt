411a2b4c0a0e88b297a1e6b72a4b7bc374562c05
Belinsky Roman <romanbelinsky99@gmail.com>
2022-01-09 16:18:55 +0600

    #10: Refactor file PasswordPolicyParser
    1. Simplify function getPasswordPolicies
    2. Refactor tests in class PasswordPolicyParserTest

--- src/main/kotlin/com/rombe/advnt/of/code/year2020/day2/PasswordPolicyParser.kt
+++ src/main/kotlin/com/rombe/advnt/of/code/year2020/day2/PasswordPolicyParser.kt
@@ -1,34 +1,15 @@
 package com.rombe.advnt.of.code.year2020.day2
 
-import java.io.FileNotFoundException
-import java.nio.file.Files
 import java.nio.file.Path
-import java.util.*
-import kotlin.io.path.notExists
+import kotlin.io.path.useLines
 
-fun getPasswordPolicies(fileName: String): List<PasswordPolicy> {
-    val pathToData = Path.of(fileName)
-
-    if (pathToData.notExists()) {
-        throw FileNotFoundException("File by $pathToData not found")
-    }
-
-    val result = LinkedList<PasswordPolicy>()
-
-    Files.newBufferedReader(pathToData)?.use { reader ->
-        var line = reader.readLine()
-
-        while (line != null) {
-            result.add(line.toPasswordPolicy(dataSeparator = " ", rangeSeparator = "-"))
-
-            line = reader.readLine()
-        }
-    }
-
-    return result
+fun getPasswordPolicies(filePath: Path) = filePath.useLines { lines ->
+    lines
+        .map { line -> line.toPasswordPolicy(dataSeparator = " ", rangeSeparator = "-") }
+        .toList()
 }
 
-private fun String.toPasswordPolicy(dataSeparator: String, rangeSeparator: String): PasswordPolicy {
+fun String.toPasswordPolicy(dataSeparator: String, rangeSeparator: String): PasswordPolicy {
     val data = split(dataSeparator)
     val range = data[0].split(rangeSeparator).let {
         it[0].toInt()..it[1].toInt()
--- src/test/kotlin/com/rombe/advnt/of/code/year2020/day2/PasswordPolicyParserTest.kt
+++ src/test/kotlin/com/rombe/advnt/of/code/year2020/day2/PasswordPolicyParserTest.kt
@@ -2,13 +2,14 @@ package com.rombe.advnt.of.code.year2020.day2
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.nio.file.Path
 
 class PasswordPolicyParserTest : StringSpec({
+    val pathToData = Path.of("./src/test/resources/year2020/day2/input.txt")
 
     "Correct passwords count" {
         val expectedCorrectPasswordsAmount = 434
-        val actualCorrectPasswordsAmount = getPasswordPolicies("./src/test/resources/year2020/day2/input.txt")
-            .count(PasswordPolicy::isValid)
+        val actualCorrectPasswordsAmount = getPasswordPolicies(pathToData).count(PasswordPolicy::isValid)
 
         actualCorrectPasswordsAmount shouldBe expectedCorrectPasswordsAmount
     }
